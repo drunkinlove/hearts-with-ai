9bc3cb862a1a258f2e7bb166d899f344b2d6d7d6
noname <none@whatever>
2025-08-23 21:12:30 +0400

    DumbPlayer now more whimsical

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Playing card pictures from https://code.google.com/archive/p/vector-playing-cards/
-Back from https://commons.wikimedia.org/wiki/File:Tiled_blue_broken_line_rhombus_card_back.svg
+I wanted no external dependencies. Could use a refactor
+
+Resources:
+
+- Rules mostly from https://bicyclecards.com/how-to-play/hearts/
+
+- Playing card pictures from https://code.google.com/archive/p/vector-playing-cards/
+
+- Back from https://commons.wikimedia.org/wiki/File:Tiled_blue_broken_line_rhombus_card_back.svg
--- players.js
+++ players.js
@@ -39,28 +39,30 @@ export class DumbPlayer {
 
   async select_card_for_trick(hand, legal_cards, trick, cards_taken) {
     await new Promise((r) => setTimeout(r, 100));
-    const leading_suit = trick.length !== 0 ? trick[0][1][0] : null;
-    var ordered_hand = [];
-    legal_cards = legal_cards.slice();
-    const bad_cards = [];
-    for (const card of legal_cards) {
-      if (card === "♠Q") {
-        bad_cards.push(card);
-      }
-      if (card[0] === leading_suit) {
-        ordered_hand.push(card);
-      }
-      if (card[0] === "♥") {
-        bad_cards.push(card);
-      }
-    }
-    legal_cards = legal_cards.filter((card) => !ordered_hand.includes(card));
-    legal_cards = legal_cards.filter((card) => !bad_cards.includes(card));
+    const idx = sample_indices(legal_cards.length, 1)[0];
+    // const leading_suit = trick.length !== 0 ? trick[0][1][0] : null;
+    // var ordered_hand = [];
+    // legal_cards = legal_cards.slice();
+    // const bad_cards = [];
+    // for (const card of legal_cards) {
+    //   if (card === "♠Q") {
+    //     bad_cards.push(card);
+    //   }
+    //   if (card[0] === leading_suit) {
+    //     ordered_hand.push(card);
+    //   }
+    //   if (card[0] === "♥") {
+    //     bad_cards.push(card);
+    //   }
+    // }
+    // legal_cards = legal_cards.filter((card) => !ordered_hand.includes(card));
+    // legal_cards = legal_cards.filter((card) => !bad_cards.includes(card));
 
-    ordered_hand = ordered_hand.concat(legal_cards);
-    ordered_hand = ordered_hand.concat(bad_cards);
-    const played_card = ordered_hand[0];
-    return played_card;
+    // ordered_hand = ordered_hand.concat(legal_cards);
+    // ordered_hand = ordered_hand.concat(bad_cards);
+    // const played_card = ordered_hand[0];
+
+    return legal_cards[idx];
   }
 }
 
